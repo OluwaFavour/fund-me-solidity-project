4bb8b70134982bc07a0609ed7a9d67cdda27e576
OluwaFavour <fsticks8187@gmail.com>
2024-10-29 03:54:28 +0100

    Add price retrieval and price conversion functionality

--- FundMe.sol
+++ FundMe.sol
@@ -1,12 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    uint256 public minimumUSD = 5;
-    
+    uint256 public minimumUSD = 5e18;
+
     function fund() public payable {
         // Allow users to send money
         // Have a minimum amount sent
-        require(msg.value > minimumUSD); // 1e18 Wei equals 1TTH
+        require(getConversionRate(msg.value) > minimumUSD, "Didn't send enough ETH"); // 1e18 Wei equals 1ETH
+    }
+
+    function getPrice() public view returns(uint256) {
+        // ETH/USD Price Feed Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price, , , ) = priceFeed.latestRoundData(); // Price of ETH in USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 }
