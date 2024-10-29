67faa8efbd2a064d01cba4ca6e3790384c00c094
OluwaFavour <fsticks8187@gmail.com>
2024-10-29 04:31:31 +0100

    Convert Price Conversion functionalities to a library

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,27 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "libraries/PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUSD = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
         // Allow users to send money
         // Have a minimum amount sent
-        require(getConversionRate(msg.value) > minimumUSD, "Didn't send enough ETH"); // 1e18 Wei equals 1ETH
-    }
-
-    function getPrice() public view returns(uint256) {
-        // ETH/USD Price Feed Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price, , , ) = priceFeed.latestRoundData(); // Price of ETH in USD
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+        require(msg.value.getConversionRate() > minimumUSD, "Didn't send enough ETH"); // 1e18 Wei equals 1ETH
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 }
